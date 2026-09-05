9c19fa81cf0e3d39f99ea0c8dfb25eee73edc0fc
gmastellone <gmastellone94@gmail.com>
2026-09-05 21:35:40 +0200

    chore: scaffold Varco project (Hono + Workers + vitest)

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export default {
+  async fetch(): Promise<Response> {
+    return new Response("not implemented yet", { status: 501 });
+  },
+};
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface Bindings {
+  FILES_KV: KVNamespace;
+  ASSETS: Fetcher;
+  B2_KEY_ID: string;
+  B2_APP_KEY: string;
+  B2_BUCKET: string;
+  B2_ENDPOINT: string;
+  B2_REGION: string;
+}
